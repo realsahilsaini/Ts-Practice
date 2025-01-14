a878416666f53e2407aec74c8b8ed3006d17222e
realsahilsaini <thisissahilsaini@gmail.com>
2025-01-14 14:03:43 +0400

    Interfaces

---
+++ Ts/Interfaces.ts
@@ -0,0 +1,22 @@
+//Interfaces and Type Aliases
+//Defining Interface 
+//Using Interfaces to define the structure of an object
+//Extending interfaces
+//Types Aliases
+//Intersection types
+
+
+//Interface 
+
+//It is a way to define a structure of an object
+interface Person {
+    name: string;
+    age: number;
+    gender?: string;
+}
+
+function getDataofUser(obj: Person){
+    //code
+}
+
+getDataofUser({name: 'Sahil', age: 22, gender:'Male'});

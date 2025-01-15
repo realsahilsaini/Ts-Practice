30edaca13e134524ed934bfa5ba22fd31292c278
realsahilsaini <thisissahilsaini@gmail.com>
2025-01-15 14:47:49 +0400

    this keyword

--- Ts/Class.ts
+++ Ts/Class.ts
@@ -1,3 +1,14 @@
+//Calsses and Objects 
+//Class definition 
+//Contructors 
+//Access modifiers (public, private, protected)
+// Read only properties
+//Optional Properties
+//Parameter properties
+//Getters and Setters
+//Static members
+//Abstract classes and methods
+
 class BottleMaker{
   constructor(
     public name: string, public price: number, public color: string, public material: string
@@ -12,5 +23,14 @@ let b2 = new BottleMaker('Cello', 500,  'Blue', 'plastic');
 
 console.log(b1);
 console.log(b2);
+
+//this Keyword
+class Person{
+  constructor(public name: string, public age: number){
+    console.log(this.name + ' ' + this.age);
+  }
+}
+
+
 
 

35bc13506aee4ac264b6cf24229dbb0163ca66ba
realsahilsaini <thisissahilsaini@gmail.com>
2025-01-19 13:24:56 +0530

    Generics Info

--- Ts/Generics.ts
+++ Ts/Generics.ts
@@ -29,5 +29,11 @@ The <T> after the function name indicates that the function is a generic functio
 It can accept any type of data.
 
 The <T> is a placeholder for the type that will be provided by the caller of the function.
+
+The <datatype> is optional, if not provided then TypeScript will try to infer the type from the argument passed to the function.
+
+displayData<string>("Hello") - Here, we are explicitly specifying the type of the argument as string.
+
+displayData("Hello") - Here, we are not specifying the type of the argument. TypeScript will infer the type from the argument passed to the function.
 */
 

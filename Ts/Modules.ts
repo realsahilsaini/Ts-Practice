5056195ad8dbeb6db8430e7d59d7aa6f2f199d3c
realsahilsaini <thisissahilsaini@gmail.com>
2025-01-19 13:47:14 +0530

    Modules

---
+++ Ts/Modules.ts
@@ -0,0 +1,15 @@
+export function addPaymentMethod(){
+    console.log("Payment method added");
+}
+
+export function getDetails(){
+    console.log("Payment method removed");
+}
+
+
+
+export default class MyModulesClass{
+    constructor(){
+        console.log("MyModulesClass constructor");
+    }
+}
---
+++ Ts/ModulesUsage.ts
@@ -0,0 +1,12 @@
+import {getDetails, addPaymentMethod} from './Modules'
+
+import MyModulesClass from './Modules'
+
+addPaymentMethod();
+getDetails();
+
+let myModulesClass = new MyModulesClass();
+
+
+
+

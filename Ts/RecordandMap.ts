b898ea77cb4485316e94b36a950758b73377f707
realsahilsaini <thisissahilsaini@gmail.com>
2025-02-09 17:14:12 +0530

    Record

---
+++ Ts/RecordandMap.ts
@@ -0,0 +1,50 @@
+//You can type objects like follows - 
+// type MapUser = {
+//     id: string;
+//     name: string;
+//     age: number;
+// }
+
+// type MapUsers = {
+//   [key: string]: MapUser;
+// }
+
+
+// const mapsUsers: MapUsers = {
+//   'id1':{
+//     id: '1',
+//     name: 'Sahil',
+//     age: 22
+//   },
+//   'id2':{
+//     id: '2',
+//     name: 'Aaradhya',
+//     age: 13
+//   }
+// }
+
+// console.log(mapsUsers['id1'].name) // Sahil
+
+
+//Using Record
+type MapUser = {
+    id: string;
+    name: string;
+    age: number;
+}
+
+//Syntax: Record<keyType, valueType>
+const mapsUsers: Record<string, MapUser> = {
+  'id1':{
+    id: '1',
+    name: 'Sahil',
+    age: 22
+  },
+  'id2':{
+    id: '2',
+    name: 'Aaradhya',
+    age: 13
+  }
+}
+
+console.log(mapsUsers['id1'].name) // Sahil

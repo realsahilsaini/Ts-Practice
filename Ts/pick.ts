13d021af55c099c6e23afce07cbced11d471f59e
realsahilsaini <thisissahilsaini@gmail.com>
2025-02-09 11:09:52 +0530

    Pick

---
+++ Ts/pick.ts
@@ -0,0 +1,22 @@
+interface PickUser{
+  id: string;
+  name: string;
+  age: number;
+  email: string;
+  password: string;
+}
+
+// Here we have an interface PickUser with 5 properties. We want to create a new type that only has 3 properties from the PickUser interface. We can use the Pick utility type to do this.
+type UpdatePickUser = Pick<PickUser, 'id' | 'name' | 'age'>;
+
+function updateUser(obj: UpdatePickUser){
+  console.log(obj);
+}
+
+
+updateUser({
+  id: '1',
+  name: 'Sahil',
+  age: 22
+})
+
